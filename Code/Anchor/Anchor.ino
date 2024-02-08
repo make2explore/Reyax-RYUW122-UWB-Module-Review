42e7b227bc398e0e117517fc0ed009008ccddc3e
make2explore <info@make2explore.com>
2024-02-08 20:14:27 +0530

    Update Anchor.ino

--- Code/Anchor/Anchor.ino
+++ Code/Anchor/Anchor.ino
@@ -106,8 +106,8 @@ unsigned long previousTime = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial1.begin(115200);
+  Serial.begin(115200);          // Serial is for getting logs of code execution
+  Serial1.begin(115200);         // Serial1 is connected to RYUW122 Module
 
   Serial.println("Starting....");
   Serial.println("");
